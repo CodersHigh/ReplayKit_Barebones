5ebb47b7d1b3abcd71a88e69ae08bd4937c48816
이로운 <lru040409@gmail.com>
2022-07-29 09:56:15 +0900

    프로젝트 셋업

---
+++ ReplayKit_Barebones/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  ReplayKit_Barebones
+//
+//  Created by 이로운 on 2022/07/29.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ ReplayKit_Barebones/ReplayKit_BarebonesApp.swift
@@ -0,0 +1,17 @@
+//
+//  ReplayKit_BarebonesApp.swift
+//  ReplayKit_Barebones
+//
+//  Created by 이로운 on 2022/07/29.
+//
+
+import SwiftUI
+
+@main
+struct ReplayKit_BarebonesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
